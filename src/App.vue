df897fecc46546afc81729358a8d850ca80a5df3
Ezra Atmanda <ezraatmnda@gmail.com>
2023-12-05 09:20:23 +0700

    [ADD] 404 modal

---
+++ src/App.vue
@@ -0,0 +1,29 @@
+<template>
+  <div class="container">
+    <button @click="open = true" class="mybutton green">
+      <span class="front green">
+        CLICK ME
+      </span>
+    </button>
+
+    <Teleport to="body">
+      <Modal :show="open" @close="open = false" />
+    </Teleport>
+  </div>
+</template>
+
+<script setup>
+import Modal from './views/content/index.vue';
+import { ref } from 'vue'
+
+const open = ref(false)
+</script>
+
+<style scoped>
+.modal {
+  position: fixed; /* or absolute, depending on your layout */
+  z-index: 2; /* make sure the modal appears above the button */
+  width: 100%;
+  height: 100%;
+}
+</style>
---
+++ src/views/content/index.vue
@@ -0,0 +1,65 @@
+<template>
+  <Transition>
+    <div v-if="show" class="modal-mask">
+      <div class="modal-wrapper">
+        <div class="modal-container">
+          <h3>
+            ERROR 404
+          </h3>
+          <hr class="divider"/>
+          <button @click="$emit('close')" class="mybutton white">
+            <span class="front white">
+              Close
+            </span>
+          </button>
+        </div>
+      </div>
+    </div>
+  </Transition>
+</template>
+
+<script>
+export default {
+  name: 'Modal',
+};
+</script>
+
+<script setup>
+const props = defineProps({
+  show: Boolean
+})
+</script>
+
+<style>
+.modal-mask {
+  position: fixed;
+  z-index: 9998;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
+  background-color: rgba(0, 0, 0, 0.5);
+  display: table;
+  transition: opacity 0.3s ease;
+}
+.modal-wrapper {
+  display: table-cell;
+  vertical-align: middle;
+}
+.modal-container {
+  font-family: press2p;
+  width: 300px;
+  margin: 0px auto;
+  padding: 20px 30px;
+  background-color: #731112;
+  color: white;
+  border-radius: 2px;
+  box-shadow: 0 2px 8px rgba(0, 0, 0, 0.33);
+  transition: all 0.3s ease;
+  text-align: center;
+}
+.divider {
+  color: white;
+  margin-bottom: 20px;
+}
+</style>
